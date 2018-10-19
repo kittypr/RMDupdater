ac8e4642b2fe2efe2d1816286393ba4f6d474b9c
Julia <julettazhuk@gmail.com>
2018-10-19 17:53:17 +0700

    algorithm chosen, documentation added

--- RMDupdater/lcs.py
+++ RMDupdater/lcs.py
@@ -1,49 +1,29 @@
-import time
 
-def by_LCS(X, Y):
-    m = len(X)
-    n = len(Y)
-    # An (m+1) times (n+1) matrix
+def longest_common_subsequence(first_seq, second_seq):
+    """Computes the LCS between two sequences.
+
+    The function below takes as input two sequences, computes the LCS between them,
+    and stores it in C[i,j]. C[m,n] will contain indexes of first_seq which contain the LCS.
+    Both sequences should have comparable elements. Complexity O(n*m)
+
+    :param first_seq: iterable - first sequence.
+    :param second_seq: iterable - second sequence.
+    :return: list of indexes of first_seq which are not included on LCS
+    """
+    m = len(first_seq)
+    n = len(second_seq)
+    # An (m+1) times (n+1) matrix of empty lists
     C = [[list()] * (n + 1) for _ in range(m + 1)]
-    st = time.time()
     for i in range(1, m+1):
         for j in range(1, n+1):
-            if X[i-1] == Y[j-1]:
+            if first_seq[i - 1] == second_seq[j - 1]:
                 C[i][j] = [i-1, ]
                 C[i][j].extend(C[i-1][j-1])
             else:
                 C[i][j] = (max(C[i-1][j], C[i][j-1], key=len))
-    with open('time_log.txt', 'a+') as log:
-        log.write("LCS: " + str(st-time.time()) + "\n" + str(C[m][n]) + "\n")
-    return C
-
-def by_sort(x, y):
-    x.sort()
-    y.sort()
-    res = list()
-    st = time.time()
-    for i in range(0, len(x)):
-        for j in range(0, len(y)):
-            if x[i] == y[j]:
-                res.append(i)
-                break
-            elif x[i] < y[j]:
-                break
-    with open('time_log.txt', 'a+') as log:
-        log.write("SORT: " + str(st-time.time()) + "\n" + str(res) + "\n")
-    return res
-
-# X = ['hello ', 'my ', 'dear ', 'friend, ', 'I`m ', 'Julia.']
-# Y = ['my ', 'dear ', 'friend, ', 'hello!']
-#
-# m = len(X)
-# n = len(Y)
-# C = by_LCS(X, Y)
-# res = C[m][n]
-# for i in res:
-#     print(X[i])
-#
-# st = time.time()
-# res = by_sort(X, Y)
-# for i in res:
-#     print(X[i])
+    lcs = C[m][n]
+    result = list()
+    for i in range(0, m):
+        if i not in lcs:
+            result.append(i)
+    return result
